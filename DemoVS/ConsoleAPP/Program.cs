8759b82f90c6ebe6053f35798e24626c77a06cb4
ecardos1 <edersoncard@gmail.com>
2019-03-19 23:23:43 -0400

    First Commit

---
+++ DemoVS/ConsoleAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo integration VS with github");
+            Console.ReadKey();
+        }
+    }
+}
